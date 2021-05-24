648f4943f0129cb552fbf8c8c44883192f364b36
Novaras <tfear091@gmail.com>
2021-05-24 19:17:27 +0100

    init

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import './styles/vars.css';
+</script>
+
+<main>
+	<h1 style="color: var(--blue);">Hello!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
